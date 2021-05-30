630974114a216d84fcce02622cfc68c32a50362a
Yaniv Suriano <yaniv@skysoftsl.com>
2021-05-30 17:39:48 +0300

    Clean the code

--- actions/mainPageAction.js
+++ actions/mainPageAction.js
@@ -1,7 +1,6 @@
 import dataSet from "../data/dataSent.json";
 
 // -------------- Action Typs for Reducer --------- //
-export const RESULT_GET_DATA_FROM_SERVER = "RESULT_GET_DATA_FROM_SERVER";
 export const RESULT_GOOD = "RESULT GOOD";
 export const RESULT_BAD = "RESULT BAD";
 export const RESULT_SEND = "RESULT SEND";
@@ -10,14 +9,6 @@ export const RESULT_FETCH_DATA = "RESUT FETCH DATA";
 
 export const createActionSendResult = (obj) => {
   return { type: RESULT_SEND, payload: obj };
-};
-
-export const createActionGoodResult = (obj) => {
-  return { type: RESULT_GOOD, payload: obj };
-};
-
-export const createActionBadResult = (obj) => {
-  return { type: RESULT_BAD, payload: obj };
 };
 
 export const createActionFetchData = (dataSet) => {
@@ -30,3 +21,4 @@ export const loadData = () => {
     dispatch(createActionFetchData(newDataSet));
   };
 };
+ss;
